c8b3b0a8308bcc64eab15cea1f3f5c63c064f0fc
Gustavo Rios <gustavorios.gr25@gmail.com>
2026-05-12 21:29:05 -0300

    changes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/api/football', async (req, res) => {
   const { endpoint, team } = req.query;
